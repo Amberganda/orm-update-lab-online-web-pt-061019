77e31868455c7a473d0b5db5f23425486efc1d2f
Amber Taranto <ambzzzz@gmail.com>
2019-09-09 04:32:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_reader :name, :grade
+
+  def initialize
+      @name = name
+      @grade = grade
+      @id = nil
+  end
 
 end
