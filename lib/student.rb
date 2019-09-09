a2cc04e659984225353f443ba1578ac67c8c4cd3
Amber Taranto <ambzzzz@gmail.com>
2019-09-09 05:20:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,11 @@ class Student
       Student.new(id = row[0], row[1], row[2])
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+      sql = "SELECT * FROM students WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
+  end
 
 
   end
